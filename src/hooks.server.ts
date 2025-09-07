c62776d06a6d05c34d9da7f91d9651123f0971a1
zarir94 <shahriarzariracc@gmail.com>
2025-09-07 15:04:34 +0600

    set ip find

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,27 @@
+//@ts-nocheck
 import type { Handle } from '@sveltejs/kit';
+import type { RequestEvent } from './routes/$types';
+
+function getClientIP(event: RequestEvent) {
+	let headers = event.request.headers;
+
+	// Cloudflare
+	let cf = headers.get('cf-connecting-ip');
+	if (cf) return cf;
+
+	// Vercel / Netlify / generic proxy
+	let xff = headers.get('x-forwarded-for');
+	if (xff) return xff.split(',')[0].trim();
+
+	// Default (SvelteKit built-in)
+	return event.getClientAddress();
+}
 
 export const handle: Handle = async ({ event, resolve }) => {
-	let ip = event.getClientAddress();
+	let ip = getClientIP(event);
+	event.setHeaders('X-User-IP', ip)
+	event.locals.clientIP = ip;
 	let response = await resolve(event);
-	response.headers.set('X-User-IP', ip);
 	return response;
 };
 
