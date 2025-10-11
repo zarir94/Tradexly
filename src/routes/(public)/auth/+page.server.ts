bf1f70ccc8bfd6e3c900ffa2f213eabf52ee7db2
zarir94 <shahriarzariracc@gmail.com>
2025-10-11 11:11:58 +0600

    add fallback

--- src/routes/(public)/auth/+page.server.ts
+++ src/routes/(public)/auth/+page.server.ts
@@ -1,5 +1,5 @@
-//@ts-nocheck
-import { redirect, type Actions } from "@sveltejs/kit";
+//@ts-nochecks
+import { fail, redirect, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import prisma from "$main/src/lib/prisma";
 import { comparePassword, getSettings, hashPassword, isEmailAcceptable, isUsernameOK } from "$main/src/lib/func";
@@ -8,6 +8,30 @@ function daysFromNow(x: number): Date {
     const d = new Date();
     d.setDate(d.getDate() + x);
     return d;
+}
+
+async function getCityCountry(ip: string) {
+    try {
+        let r = await fetch('http://ip-api.com/json/' + ip);
+        if (!r.ok) throw new Error(`IP-Api.com Error ${r.status}: ${await r.text()}`);
+        let d = await r.json();
+        let pl: (string)[] = [d.city, d.country].filter(Boolean);
+        if (pl.length == 0) throw new Error(`IP-Api.com Error: Length Empty. ${r.status} - Resp: ${JSON.stringify(d)}`);
+        return pl;
+    } catch (err) {
+        console.error(err);
+        try {
+            let r = await fetch('http://free.freeipapi.com/api/json/' + ip);
+            if (!r.ok) throw new Error(`FreeIPapi.com Error ${r.status}: ${await r.text()}`);
+            let d = await r.json();
+            let pl: string[] = [d.cityName, d.countryName].filter(Boolean);
+            if (pl.length == 0) throw new Error(`FreeIPapi.com Error: Length Empty. ${r.status} - Resp: ${JSON.stringify(d)}`);
+            return pl;
+        } catch (err2) {
+            console.error(err2);
+            return null;
+        }
+    }
 }
 
 export const load: PageServerLoad = async ({ locals }) => {
@@ -21,20 +45,18 @@ export const actions: Actions = {
         let ip = locals.clientIP;
         let toRun = ()=>{};
         try {
-            let { username, password } = Object.fromEntries((await request.formData()).entries());
+            let { username, password } = Object.fromEntries((await request.formData()).entries()) as Record<string, string | undefined>;
+            if (!(username && password)) return fail(400, {type: 'error', message: 'The Account doesnt exists on the given username/email'});
             let u;
-            if (username?.indexOf('@')!=-1) {
+            if (username.indexOf('@')!=-1) {
                 u = await prisma.user.findFirst({ where: { email: username } })
             } else {
-                u = await prisma.user.findFirst({ where: { username } })
+                u = await prisma.user.findFirst({ where: { username: username } })
             }
-            if (!u) return {type: 'error', message: 'The Account doesnt exists on the given username/email'};
-            if (!await comparePassword(password, u.passwordHash)) return {type: 'error', message: 'Invalid Password!'};
+            if (!u) return fail(400, {type: 'error', message: 'The Account doesnt exists on the given username/email'});
+            if (!await comparePassword(password, u.passwordHash)) return fail(400, {type: 'error', message: 'Invalid Password!'});
 
-            let loc = null;
-            try {
-                loc = await fetch('http://ip-api.com/json/' + ip).then((r) => r.json()).then((d)=>([d.city, d.country].filter(Boolean).join(', ') || null));
-            } catch (err) {}
+            let loc = (await getCityCountry(ip))?.join(', ') || null;
             
             let ses = await prisma.session.create({ data: { userId: u.id, token: crypto.randomUUID(), user_agent: locals.userAgent, ip_address: locals.clientIP, location: loc, expiresAt: daysFromNow(30) } })
 
@@ -51,19 +73,17 @@ export const actions: Actions = {
             let { site_name } = await getSettings('site_name');
             let affID = cookies.get('affID') || null;
             let ip = locals.clientIP;
-            let { fullName, username, email, password, passwordConfirm } = Object.fromEntries((await request.formData()).entries());
-            if (fullName?.split(' ').length < 2) return {type: 'error', message: 'Full Name must have at least 2 parts'};
+            let { fullName, username, email, password, passwordConfirm } = Object.fromEntries((await request.formData()).entries()) as Record<string, string | undefined>;
+            if (!(fullName && username && email && password && passwordConfirm)) return fail(400, { type: 'error', message: 'Please fill up all fields' });
+            if (fullName.split(' ').length < 2) return {type: 'error', message: 'Full Name must have at least 2 parts'};
             if (password != passwordConfirm) return {type: 'error', message: 'Password and Confirm Password does not match'};
-            if (password?.length < 6) return {type: 'error', message: 'Password must be atleast 6 chars long'};
-            if (!isUsernameOK(username.toString())) return {type: 'error', message: 'Username must start with a letter and contain 3-30 characters (letters, numbers, ., _, -)'};
+            if (password.length < 6) return {type: 'error', message: 'Password must be atleast 6 chars long'};
+            if (!isUsernameOK(username)) return {type: 'error', message: 'Username must start with a letter and contain 3-30 characters (letters, numbers, ., _, -)'};
             if (await prisma.user.findFirst({ where: { username } })) return {type: 'error', message: 'Username already exists, choose another'};
             if (await prisma.user.findFirst({ where: { email } })) return {type: 'error', message: 'This email is already registered'};
             let esr = await isEmailAcceptable(email);
             if (esr == false) return {type: 'error', message: 'The provided email is not acceptable. Email may be disposable or fake.'};
-            let country = null;
-            try {
-                country = await fetch('http://ip-api.com/json/' + ip).then(r=>r.json()).then(d=>d.country || null);
-            } catch (err) {}
+            let country = (await getCityCountry(ip))?.at(1) || null;
 
             await prisma.$transaction(async (tx) => {
                 let user = await tx.user.create({ data: { fullName, username, email, country, affID, passwordHash: await hashPassword(password) } });
